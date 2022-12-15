56cb5c744c90314eec181b7557767af0919f4a73
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-15 12:58:45 +0330

    Fix namespace and Add role observer'

--- src/AclEventServiceProvider.php
+++ src/AclEventServiceProvider.php
@@ -1,14 +1,17 @@
 <?php
 
-namespace App\acl\acl\src;
+namespace Celysium\ACL;
 
-use App\acl\acl\src\Observers\PermissionObserver;
+use Celysium\ACL\Models\Role;
+use Celysium\ACL\Observers\PermissionObserver;
 use Celysium\ACL\Models\Permission;
+use Celysium\ACL\Observers\RoleObserver;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider;
 
 class AclEventServiceProvider extends EventServiceProvider
 {
     protected $observers = [
-        Permission::class => [PermissionObserver::class]
+        Permission::class => [PermissionObserver::class],
+        Role::class => [RoleObserver::class]
     ];
 }
