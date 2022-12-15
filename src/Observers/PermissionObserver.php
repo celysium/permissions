27ec42f234fa88857302b56f9ff0f1ce9f44b6e4
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-15 16:21:06 +0330

    Use model methods to refresh the cache

--- src/Observers/PermissionObserver.php
+++ src/Observers/PermissionObserver.php
@@ -3,37 +3,21 @@
 namespace Celysium\ACL\Observers;
 
 use Celysium\ACL\Models\Permission;
-use Illuminate\Support\Facades\Cache;
 
 class PermissionObserver
 {
-        public function created(Permission $permission): void
-        {
-            $this->refreshAclCache($permission);
-        }
+    public function created(Permission $permission): void
+    {
+        $permission->refreshCache();
+    }
 
-        public function updated(Permission $permission): void
-        {
-            $this->refreshAclCache($permission);
-        }
+    public function updated(Permission $permission): void
+    {
+        $permission->refreshCache();
+    }
 
-        public function deleted(Permission $permission): void
-        {
-            foreach ($permission->roles as $role) {
-                $rolePermissions = $role->permissions()
-                    ->pluck('name')
-                    ->toArray();
-
-                Cache::store(env('ACL_CACHE_DRIVER', 'database'))
-                    ->put($role->name, $rolePermissions);
-            }
-        }
-
-        protected function refreshAclCache(Permission $permission): void
-        {
-            foreach ($permission->roles as $role) {
-                Cache::store(env('ACL_CACHE_DRIVER', 'database'))
-                    ->put($role->name, $permission->name);
-            }
-        }
+    public function deleted(Permission $permission): void
+    {
+        $permission->refreshCacheOnDelete();
+    }
 }
--- src/Observers/RoleObserver.php
+++ src/Observers/RoleObserver.php
@@ -3,33 +3,21 @@
 namespace Celysium\ACL\Observers;
 
 use Celysium\ACL\Models\Role;
-use Illuminate\Support\Facades\Cache;
 
 class RoleObserver
 {
     public function created(Role $role): void
     {
-        $this->refreshAclCache($role);
+        $role->refreshCache();
     }
 
     public function updated(Role $role): void
     {
-        $this->refreshAclCache($role);
+        $role->refreshCache();
     }
 
     public function deleted(Role $role): void
     {
-        Cache::store(env('ACL_CACHE_DRIVER', 'database'))
-            ->forget($role->name);
-    }
-
-    protected function refreshAclCache(Role $role): void
-    {
-        $permissions = $role->permissions()
-            ->pluck('name')
-            ->toArray();
-
-        Cache::store(env('ACL_CACHE_DRIVER', 'database'))
-            ->put($role->name, $permissions);
+        $role->refreshCacheOnDelete();
     }
 }
