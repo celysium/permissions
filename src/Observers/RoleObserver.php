08784c3a0803832d9b7f8f2c505adaf47f50bd8c
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-15 12:59:54 +0330

    Add role observer class

--- src/Observers/RoleObserver.php
+++ src/Observers/RoleObserver.php
@@ -1,8 +1,35 @@
 <?php
 
-namespace App\acl\acl\src\Observers;
+namespace Celysium\ACL\Observers;
+
+use Celysium\ACL\Models\Role;
+use Illuminate\Support\Facades\Cache;
 
 class RoleObserver
 {
-    //
+    public function created(Role $role): void
+    {
+        $this->refreshAclCache($role);
+    }
+
+    public function updated(Role $role): void
+    {
+        $this->refreshAclCache($role);
+    }
+
+    public function deleted(Role $role): void
+    {
+        Cache::store(env('ACL_CACHE_DRIVER', 'database'))
+            ->forget($role->name);
+    }
+
+    protected function refreshAclCache(Role $role): void
+    {
+        $permissions = $role->permissions()
+            ->pluck('name')
+            ->toArray();
+
+        Cache::store(env('ACL_CACHE_DRIVER', 'database'))
+            ->put($role->name, $permissions);
+    }
 }
