35363fd08ce742e2c3f67c79828b08ee5de49b50
nasser <hekmati.nasser@gmail.com>
2022-12-16 14:01:01 +0330

    add role crud api

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Celysium\ACL\Controllers\RoleController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('api')->prefix('api')->group(function () {
+
+    Route::name('acl')->middleware(config('acl.route_middlewares'))->group(function () {
+
+        Route::apiResource('/roles', RoleController::class);
+    });
+});
