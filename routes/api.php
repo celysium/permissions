7ceace7400aec65104545357d92ba0af8b823732
nasser <hekmati.nasser@gmail.com>
2022-12-16 15:39:13 +0330

    feat permission crud api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Celysium\ACL\Controllers\PermissionController;
 use Celysium\ACL\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,6 @@ Route::middleware('api')->prefix('api')->group(function () {
     Route::name('acl')->middleware(config('acl.route_middlewares'))->group(function () {
 
         Route::apiResource('/roles', RoleController::class);
+        Route::apiResource('/permissions', PermissionController::class);
     });
 });
