26dcef9093da8dbf61b6de9489961734bf40567e
Ehsan Tavakoli <ehsantavakoli1995@gmail.com>
2022-12-20 16:37:05 +0330

    Define registerGate method and use it in boot method and also add permission gate

--- src/ACLServiceProvider.php
+++ src/ACLServiceProvider.php
@@ -6,7 +6,7 @@ use Celysium\ACL\Models\Permission;
 use Celysium\ACL\Models\Role;
 use Celysium\ACL\Observers\PermissionObserver;
 use Celysium\ACL\Observers\RoleObserver;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,20 +14,15 @@ class ACLServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
 
         $this->publishes([
             __DIR__ . '/../config/acl.php' => config_path('acl.php'),
         ], 'acl-config');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        Gate::define('role', function (string $roles) {
-            $user = Auth::user();
-            $roles = explode('|', $roles);
-
-            return $user->hasRoles($roles);
-        });
+        $this->registerGates();
     }
 
     public function register()
@@ -39,6 +34,39 @@ class ACLServiceProvider extends ServiceProvider
         $this->booting(function () {
             Permission::observe([PermissionObserver::class]);
             Role::observe([RoleObserver::class]);
+        });
+    }
+
+    protected function registerGates()
+    {
+        Gate::define('role', function ($user, $role) {
+            $roles = explode('|', $role);
+
+            $userRoles = Cache::store(config('acl.cache_driver'))
+                ->remember("acl.role.$user->id", config('acl.cache_time'), function () use ($user) {
+                    return $user->roles()
+                        ->pluck('name')
+                        ->toArray();
+                });
+
+            return $user->hasRoles($roles);
+        });
+
+        Gate::define('permission', function ($user, $permission) {
+            $permissions = explode('|', $permission);
+
+            if (env('SHOP_MODE', 'light') === 'enterprise') {
+                //
+            }
+
+            $userRoles = Cache::store(config('acl.cache_driver'))
+                ->remember("acl.permission.$user->id", config('acl.cache_time'), function () use ($user) {
+                    return $user->permissions()
+                        ->pluck('name')
+                        ->toArray();
+                });
+
+            return $user->hasPermissions($permissions);
         });
     }
 }
