7e72ae9af28ec4aa8a16a7b2a7f51d23b2055abb
nasser <hekmati.nasser@gmail.com>
2023-02-03 22:40:05 +0330

    improve service provider

--- src/ACLServiceProvider.php
+++ src/ACLServiceProvider.php
@@ -2,44 +2,68 @@
 
 namespace Celysium\ACL;
 
-use App\Http\Kernel;
 use Celysium\ACL\Middleware\CheckPermission;
 use Celysium\ACL\Middleware\CheckRole;
 use Celysium\ACL\Models\Permission;
 use Celysium\ACL\Models\Role;
 use Celysium\ACL\Observers\PermissionObserver;
 use Celysium\ACL\Observers\RoleObserver;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class ACLServiceProvider extends ServiceProvider
 {
-    public function boot(Kernel $kernel)
+    public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+        $this->loadRoutes();
 
-        $this->publishes([
-            __DIR__ . '/../config/acl.php' => config_path('acl.php'),
-        ], 'acl-config');
+        $this->publishConfig();
 
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrations();
 
-        $kernel->pushMiddleware(CheckPermission::class);
-        $kernel->pushMiddleware(CheckRole::class);
+        $this->registerMiddlewares();
 
         $this->registerGates();
     }
 
     public function register()
     {
+        $this->registerConfig();
+
+        $this->registerObservers();
+    }
+
+    public function loadRoutes()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+    }
+
+    public function publishConfig()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/acl.php' => config_path('acl.php'),
+        ], 'acl-config');
+    }
+
+    public function loadMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+
+    public function registerMiddlewares()
+    {
+        /** @var Router $router */
+        $router = $this->app->make(Router::class);
+        $router->aliasMiddleware('permission', CheckPermission::class);
+        $router->aliasMiddleware('role', CheckRole::class);
+    }
+
+    public function registerConfig()
+    {
         $this->mergeConfigFrom(
             __DIR__ . '/../config/acl.php', 'acl'
         );
-
-        $this->booting(function () {
-            Permission::observe([PermissionObserver::class]);
-            Role::observe([RoleObserver::class]);
-        });
     }
 
     protected function registerGates()
@@ -54,6 +78,14 @@ class ACLServiceProvider extends ServiceProvider
             $permissions = explode('|', $permission);
 
             return $user->hasPermissionsCache($permissions);
+        });
+    }
+
+    public function registerObservers()
+    {
+        $this->booting(function () {
+            Permission::observe([PermissionObserver::class]);
+            Role::observe([RoleObserver::class]);
         });
     }
 }
