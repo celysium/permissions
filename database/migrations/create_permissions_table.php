64c86f777ac838e703475ad5c636cbc9965efeaf
nasser <hekmati.nasser@gmail.com>
2023-02-10 14:31:36 +0330

    fix permission table migrate

--- database/migrations/create_permissions_table.php
+++ database/migrations/create_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePermissionsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -57,4 +57,4 @@ class CreatePermissionsTable extends Migration
         Schema::dropIfExists('permission_roles');
         Schema::dropIfExists('permissions');
     }
-}
+};
