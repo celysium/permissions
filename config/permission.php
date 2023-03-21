7dad1f4a75792f02dc9bf65fbb15cf26cd79ce26
nasser <hekmati.nasser@gmail.com>
2023-03-21 12:24:51 +0330

    fix: config user

--- config/permission.php
+++ config/permission.php
@@ -2,15 +2,15 @@
 
 return [
     'cache' => [
-        'driver' => env('Permission_CACHE_DRIVER', 'database'),
-
+        'driver'   => env('Permission_CACHE_DRIVER', 'database'),
         'lifetime' => env('Permission_CACHE_LIFETIME', 60),
     ],
 
     'user' => [
-        'model' => \App\Models\User::class,
-        'table' => 'users',
-        'foreign_key' => 'users',
+        'model'       => \App\Models\User::class,
+        'table'       => 'users',
+        'foreign_key' => 'user_id',
+        'relation_id' => 'id',
+        'type'        => 'unsignedBigInteger'
     ]
-
 ];
