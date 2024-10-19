30e408e470815eb316a3e831ab75010d5918f91b
nasser <hekmati.nasser@gmail.com>
2024-10-19 09:41:56 +0330

    fix: condition permission repository

--- src/Repositories/Permission/PermissionRepository.php
+++ src/Repositories/Permission/PermissionRepository.php
@@ -8,16 +8,12 @@ use Illuminate\Database\Eloquent\Builder;
 
 class PermissionRepository extends BaseRepository implements PermissionRepositoryInterface
 {
-    public function __construct(Permission $permission)
-    {
-        parent::__construct($permission);
-    }
+    protected static string $entity = Permission::class;
 
     public function conditions(Builder $query): array
     {
         return [
-            'name'  => fn($value) => $query->where('name', 'like', "%$value%"),
-            'title' => fn($value) => $query->where('title', 'like', "%$value%"),
+            'name' => 'like',
         ];
     }
 }
